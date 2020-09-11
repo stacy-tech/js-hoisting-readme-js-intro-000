af643c7c1d8e338dd8c35461e5b163d39bc44fc2
Stacy Daniel <stacydn23@gmail.com>
2020-09-11 21:03:14 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -3,28 +3,45 @@ function callMe() {
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
-  return lyric;
   var lyric = "maybe";
+  return lyric;
+  callme()
 }
-
 
 function crazy() {
-  // fix the code in here:
+  //fix the code in here:
   thisIsCrazy();
 
-  var thisIsCrazy = function (){
-    console.log("hey!!!")
+    var thisIsCrazy = function (){
+      console.log("hey!!!")
   }
 }
+var crazy = function() {
+  var thisIsCrazy = function(){
+    console.log("hey!!!")
+  }
+  thisIsCrazy();
+}
+crazy()
 
 function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
 
-  sayMy();
-
-  function sayMy() {
-    console.log(name);
-    var name = "Kristin";
+function sayMy() {
+  name = "Kristin";
+    console. log(name);
   }
+  sayMy();
 }
+sayMyName()()
+var sayMyName = function(){
+  var name;
+  name = "Cricky";
+  function sayMy(){
+    name = "Kristin";
+    console. log(name);
+}
+sayMy();
+}
+sayMyName()()
